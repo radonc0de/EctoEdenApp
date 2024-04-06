64f47e9a2a42c6f354a9ecc8eb73b5e14f4dd03d
Keith Lehman <kpl56@carr026.nb-labs.rutgers.edu>
2024-04-06 15:18:24 -0400

    Initial Commit

---
+++ Application/ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplicationApp.swift
+//  Application
+//
+//  Created by Keith Lehman on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
